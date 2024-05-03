8becf500cee4fe41ef95665780fe52eb598aa001
Stiven Leite <stivenleite@gmail.com>
2024-05-03 14:44:46 -0300

    facebook link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import logo from "../assets/logo.svg";
 import { FaX, FaBars } from "react-icons/fa6";
 import { isMobile, osName } from "react-device-detect";
